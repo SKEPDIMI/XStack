3d3a58087d1e46888fed9d65b60215699ee61ea1
John S <diaz.john312@gmail.com>
2018-10-01 19:41:47 -0400

    Added responsive image for smaller devices

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,20 +1,56 @@
 <template>
   <div class="container">
-    <img src='../assets/ipad.png' />
+    <img :src="deviceName" />
   </div>
 </template>
 
 <script>
+const pathToImg = (n) => {
+  return './static/images/' + n + ".png"
+};
+const getImgUrl = () => window.innerWidth > 1000 ? pathToImg('ipad') : pathToImg('phone');
+
 export default {
   name: 'Device',
+  mounted() {
+    const self = this;
+    self.$nextTick(function() {
+      window.onresize = function() {
+        self.setDevice('set-device', getImgUrl());
+      }
+    });
+  },
+  data() {
+    return {
+      deviceName: getImgUrl()
+    }
+  },
+  methods: {
+    setDevice() {
+      this.deviceName = getImgUrl()
+    },
+  },
 };
 </script>
 
 <style scoped>
 .container {
-
+  display: flex;
+  align-items: flex-end;
 }
 img {
-  height: 50vh;
+  height: 18rem;
+}
+
+@media screen and (max-width: 1000px) {
+  img {
+    height: 90vh;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  img {
+    display: none;
+  }
 }
 </style>
