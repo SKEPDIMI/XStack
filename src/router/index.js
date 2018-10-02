971f7ca978d913c7d49fd36d45b50c65f42edba1
John S <diaz.john312@gmail.com>
2018-10-02 19:59:49 -0400

    Added services route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Landing from '@/views/Landing';
 import About from '@/views/About';
+import Services from '@/views/Services';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: About,
+    },
+    {
+      path: '/services',
+      name: 'Services',
+      component: Services,
     },
   ],
 });
