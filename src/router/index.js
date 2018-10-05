a733fb067bcd1126c20de24792c025f97c903115
John S <diaz.john312@gmail.com>
2018-10-05 17:24:25 -0400

    added blog / team views

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router';
 import Landing from '@/views/Landing';
 import About from '@/views/About';
 import Services from '@/views/Services';
+import Team from '@/views/Team';
+import Blog from '@/views/Blog';
+import Contact from '@/views/forms/Contact';
 
 Vue.use(Router);
 
@@ -23,6 +26,21 @@ export default new Router({
       name: 'Services',
       component: Services,
     },
+    {
+      path: '/team',
+      name: 'Team',
+      component: Team
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    }
   ],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
---
+++ src/views/Team.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="team">
+    <div class="main">
+      <div>
+        <h1>We are still gathering our team.</h1>
+        <p>Please come back later or shoot us a message!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Team'
+}
+</script>
+
+<style>
+.main {
+  justify-content: center;
+}
+</style>
