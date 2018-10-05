adef320d31599b7b18d7af566e35bdba940c6604
John S <diaz.john312@gmail.com>
2018-10-04 22:57:28 -0400

    Added content for Services

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -27,19 +27,24 @@
       <div class="body" ref="body">
       <div class="row">
         <div>
-          <h2>Putting your company online and keeping it there</h2>
+          <h2>High performing, secure</h2>
           <h1>Web Development</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam laoreet purus pretium mauris finibus feugiat. Mauris a 
-            arcu porta ante tristique efficitur non non sapien. Nulla 
-            quis ipsum quis diam congue egestas. Etiam sed sodales ipsum, 
-            finibus faucibus enim. Donec finibus mauris ac nunc porta mattis. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Donec accumsan ultricies erat nec faucibus. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Etiam vitae sodales sem. Sed dapibus eros 
-            non erat auctor, sit amet mollis lacus elementum. Sed lobortis lacus 
-            in placerat aliquam.</p>
+          <p>Our developers provide web application services to our clients based off of their needs. 
+            XStack offers a variety of web development services, from responsive design, custom landing pages,
+            user experience / interaction and solutions to business logic. The appearance, accessibility,  
+            and usability of your website is more important than ever, as more and more clients make 
+            decisions based off these factors. XStack provides solutions using the latest technologies 
+            in an increasingly competitive market.</p>
+            <h3>Some of our favorite technologies</h3>
+            <ul>
+              <li>HTML5 / CSS</li>
+              <li>JavaScript (ES6)</li>
+              <li>React.js/ Redux</li>
+              <li>Vue.js</li>
+              <li>Node.js / Express</li>
+              <li>Ruby on Rails</li>
+              <li>Postgre, MongoDB, Redis</li>
+            </ul>
           <router-link to="/contact">Get started</router-link>
         </div>
         <div class="img-frame">
@@ -48,19 +53,12 @@
       </div>
       <div class="row">
         <div>
-          <h2>Making your product versatile</h2>
+          <h2>Solutions for all platforms</h2>
           <h1>Mobile Development</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam laoreet purus pretium mauris finibus feugiat. Mauris a 
-            arcu porta ante tristique efficitur non non sapien. Nulla 
-            quis ipsum quis diam congue egestas. Etiam sed sodales ipsum, 
-            finibus faucibus enim. Donec finibus mauris ac nunc porta mattis. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Donec accumsan ultricies erat nec faucibus. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Etiam vitae sodales sem. Sed dapibus eros 
-            non erat auctor, sit amet mollis lacus elementum. Sed lobortis lacus 
-            in placerat aliquam.</p>
+          <p>With a wide range of platforms to choose from, XStack aims to create a 
+            rich experience that will meet your user or business needs. We deliver these 
+            applications with technologies like React-native, which allow for the reusability 
+            of solutions and speedy delivery on your product.</p>
           <router-link to="/contact">Get started</router-link>
         </div>
         <div class="img-frame">
@@ -71,17 +69,23 @@
         <div>
           <h2>Helping your company advance</h2>
           <h1>Software Consulting</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam laoreet purus pretium mauris finibus feugiat. Mauris a 
-            arcu porta ante tristique efficitur non non sapien. Nulla 
-            quis ipsum quis diam congue egestas. Etiam sed sodales ipsum, 
-            finibus faucibus enim. Donec finibus mauris ac nunc porta mattis. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Donec accumsan ultricies erat nec faucibus. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Etiam vitae sodales sem. Sed dapibus eros 
-            non erat auctor, sit amet mollis lacus elementum. Sed lobortis lacus 
-            in placerat aliquam.</p>
+          <p>Our developers are ready to help you find solutions tailored 
+            to the needs of your business. Let us guide you as you transform 
+            your product into a digital strategy in today's competitive marketplace.
+          </p>
+          <p>Helping you build personalized experiences is what we do. XStack can help 
+            you in the following ways:
+          </p>
+          <ul>
+            <li>Consultation</li>
+            <li>Implementation</li>
+            <li>Customization</li>
+            <li>Automation</li>
+            <li>Development</li>
+            <li>Training</li>
+            <li>Creation</li>
+            <li>Maintainance</li>
+          </ul>
           <router-link to="/contact">Get started</router-link>
         </div>
         <div class="img-frame">
@@ -92,17 +96,11 @@
         <div>
           <h2>Let us handle the logic</h2>
           <h1>Business Automation</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam laoreet purus pretium mauris finibus feugiat. Mauris a 
-            arcu porta ante tristique efficitur non non sapien. Nulla 
-            quis ipsum quis diam congue egestas. Etiam sed sodales ipsum, 
-            finibus faucibus enim. Donec finibus mauris ac nunc porta mattis. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Donec accumsan ultricies erat nec faucibus. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Etiam vitae sodales sem. Sed dapibus eros 
-            non erat auctor, sit amet mollis lacus elementum. Sed lobortis lacus 
-            in placerat aliquam.</p>
+          <p>XStack can help the amount of time you spend on 
+            handling big data and repetitive tasks through automation. 
+            Be it handling data on your clients, business or application, 
+            we aim help you find better ways to increase the productivity 
+            of your company.</p>
           <router-link to="/contact">Get started</router-link>
         </div>
         <div class="img-frame">
@@ -163,15 +161,6 @@ input, textarea {
 textarea {
   resize: none;
   flex: 1;
-}
-.img-frame {
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-}
-.row .img-frame img {
-  max-width: 30vw;
-  border-radius: 5px;
 }
 
 @media screen and (max-width: 700px){
