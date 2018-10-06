d3c1a49a068acae4f240c595244ebc31641cef80
John S <diaz.john312@gmail.com>
2018-10-06 12:58:25 -0400

    added contact mailer

--- api/app/mailers/contact_mailer.rb
+++ api/app/mailers/contact_mailer.rb
@@ -1,2 +1,13 @@
 class ContactMailer < ApplicationMailer
+  default from: ENV['GMAIL_USERNAME']
+
+  def contact_email(data)
+    @name = data['name']
+    @email = data['email']
+    @phone = data['phone']
+    @message = data['message']
+    @services = data['services']
+
+    mail(to: @email, subject: "Contact email from #{@name}")
+  end
 end
