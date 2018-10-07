8329e45cd55026323afcbead8e3d78b995c9fd23
John S <diaz.john312@gmail.com>
2018-10-07 11:25:17 -0400

    added mail controller

---
+++ api/app/controllers/mailer_controller.rb
@@ -0,0 +1,19 @@
+class MailerController < ApplicationController
+  def contact
+    ContactMailer.contact_email(email_params).deliver_now
+
+    render json: { message: 'Email was sent!', email: params['email'] }, status: :ok
+  end
+
+  private
+
+  def email_params
+    params.permit(
+      :name,
+      :email,
+      :phone,
+      :services,
+      :message
+    )
+  end
+end
