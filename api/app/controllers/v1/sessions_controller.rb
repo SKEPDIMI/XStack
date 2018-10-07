9bed053d3cf902619d29bdc836352b9b78d5257c
John S <diaz.john312@gmail.com>
2018-10-07 12:56:48 -0400

    added session controller create action

--- api/app/controllers/v1/sessions_controller.rb
+++ api/app/controllers/v1/sessions_controller.rb
@@ -1,2 +1,18 @@
 class V1::SessionsController < ApplicationController
+  def create
+    @user = User.where(email: params[:email]).first
+
+    if @user.valid_password? params[:password]
+      
+      # create token for the user
+
+      render json: { message: 'Authenticated user', user: @user.as_json(only: [:email]) }, status: :created
+    else
+      render json: { message: 'Invalid password' }, status: :unauthorized
+    end
+  end
+
+  def destroy
+
+  end
 end
