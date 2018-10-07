74e137a36d81342fb8f4bedb43d43d47e2e0366f
John S <diaz.john312@gmail.com>
2018-10-07 13:00:07 -0400

    check for user before checking for valid password

--- api/app/controllers/v1/sessions_controller.rb
+++ api/app/controllers/v1/sessions_controller.rb
@@ -2,8 +2,8 @@ class V1::SessionsController < ApplicationController
   def create
     @user = User.where(email: params[:email]).first
 
-    if @user.valid_password? params[:password]
-      
+    if @user&.valid_password? params[:password]
+
       # create token for the user
 
       render json: { message: 'Authenticated user', user: @user.as_json(only: [:email]) }, status: :created
