0dc58319079db7657ae2bb25969ab6236d756d53
John S <diaz.john312@gmail.com>
2018-10-11 19:21:35 -0400

    custom POST method for sessions controller

--- api/app/controllers/sessions_controller.rb
+++ api/app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < Devise::SessionsController
-
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
   private
 
   def respond_with(resource, _opts = {})
