6480aa79a0796e171924991952f99f84a7722ff9
John S <diaz.john312@gmail.com>
2018-10-13 19:57:51 -0400

    application controller

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,25 +1,4 @@
 class ApplicationController < ActionController::API
-  # enable `respond_to`
   include ActionController::MimeResponds
-
-  def render_resource(resource)
-    if resource.errors.empty?
-      render json: resource
-    else
-      validation_error(resource)
-    end
-  end
-
-  def validation_error(resource)
-    render json: {
-      errors: [
-        {
-          status: '400',
-          title: 'Bad Request',
-          detail: resource.errors,
-          code: '100'
-        }
-      ]
-    }, status: :bad_request
-  end
+  respond_to :json
 end
