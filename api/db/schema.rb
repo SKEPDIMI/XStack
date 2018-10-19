ac0fc0dd377c1e9b5a5c59558ad18a4fb8da1967
John S <diaz.john312@gmail.com>
2018-10-18 20:23:45 -0400

    added new fields for User

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_003803) do
+ActiveRecord::Schema.define(version: 2018_10_18_201621) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 2018_10_14_003803) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
+    t.string "description"
+    t.integer "role"
+    t.string "url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
